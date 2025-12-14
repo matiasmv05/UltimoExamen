76960dffaad66a3488e31e29060769f6f1fac94f
matiasmv05 <147282209+matiasmv05@users.noreply.github.com>
2025-12-14 19:38:17 -0400

    Enhance UserQueryFilter with Swagger documentation
    Added Swagger annotations for user properties.

--- Amazon.Core/QueryFilters/UserQueryFilter.cs
+++ Amazon.Core/QueryFilters/UserQueryFilter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,23 @@ using System.Threading.Tasks;
 
 namespace Amazon.Core.QueryFilters
 {
-    public class UserQueryFilter
+    /// <summary>
+    /// Filtra los parámetros de usuarios
+    /// </summary>
+    public class UserQueryFilter : PaginationQueryFilter
     {
+        /// <summary>
+        /// Nombre del usuario
+        /// </summary>
+        /// <example>Juan Pérez</example>
+        [SwaggerSchema("Nombre del usuario")]
         public string Name { get; set; }
+
+        /// <summary>
+        /// Email del usuario
+        /// </summary>
+        /// <example>juan.perez@email.com</example>
+        [SwaggerSchema("Email del usuario")]
         public string Email { get; set; }
     }
 }
